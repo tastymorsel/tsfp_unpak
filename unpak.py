2fab0c477177fcfde842c0b5da34ae08260db409
Peter Hall <tastymorsel@mac.com>
2019-02-08 15:48:17 +0000

    - can now use a c2n file to convert crc filename to a human readable one

--- unpak.py
+++ unpak.py
@@ -1,12 +1,20 @@
 import struct, sys, gzip, os, shutil
 
 if (len(sys.argv) < 2 or sys.argv[1] == "--help"):
-    print "unpak.py [pakfilename]"
+    print "unpak.py [pakfilename] [optional_c2n_filename]"
     sys.exit(0)
 
 f = open(sys.argv[1], "rb")
 header = f.read(12)
 magic0, magic1, magic2, magic3, offset, length = struct.unpack("ccccii", header)
+
+c2n_file = {}
+if (len(sys.argv) == 3):
+    with open(sys.argv[2], "r") as c2n:
+        z = c2n.readlines()
+        for n in z:
+            crc,name = n.split()
+            c2n_file[crc] = name.replace('/', '_')
 
 if (magic0 != 'P' or magic1 != '5' or magic2 != 'C' or magic3 != 'K'):
     print "This doesn't look like a pak file."
@@ -32,9 +40,13 @@ for n in range(num_files):
 
 for fi in file_infos:
     f.seek(fi[1])
+    outname = "{0:#0{1}x}".format(fi[0], 10)
+    print ':' + outname + ':'
+    if (c2n_file.has_key(outname)):
+        outname = c2n_file[outname]
     if (fi[3] == 0):
         fd = f.read(fi[2])
-        with open("./unpak/" + hex(fi[0]), "wb") as o:
+        with open("./unpak/" + outname, "wb") as o:
             o.write(fd)
     else:
         fd = f.read(fi[3])
@@ -43,7 +55,7 @@ for fi in file_infos:
             g.close()
         with gzip.open("temp.gz", "rb") as gz:
             data = gz.read()
-            with open("./unpak/" + hex(fi[0]), "wb") as o:
+            with open("./unpak/" + outname, "wb") as o:
                 o.write(data)       
 
 f.close()
